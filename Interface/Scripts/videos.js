10139ded1cfec31c807c7d353f9148a8a1b88b01
Makky <teniolaa890@gmail.com>
2025-09-17 14:12:00 +0100

    Updated Youtube-clone
    New js-files added!

--- Interface/Scripts/videos.js
+++ Interface/Scripts/videos.js
@@ -6,5 +6,85 @@ export const videos = [{
         youtuber: 'Blackfiles',
         stats: '63K views • 11 days ago'
     }
-}];
+}, {
+    url: 'https://youtube.com/embed/Txk34bV9yRE',
+    channelPicture: 'Interface/channel-pictures/girl.png',
+    videoInfo: {
+        title: 'INTERVIEWING WATERLOO CS GRADS | graduation vlog🎓',
+        youtuber: 'cathliveslife',
+        stats: '498 views • 2 months ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/Zjl2vmy02As',
+    channelPicture: 'Interface/channel-pictures/boy.png',
+    videoInfo: {
+        title: 'A Day in the Life of a Software Engineer... WFH',
+        youtuber: 'PIRATE KING',
+        stats: '9.6M views • 4 years ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/13p2d-49wz8',
+    channelPicture: 'Interface/channel-pictures/professor.png',
+    videoInfo: {
+        title: 'Democrats TERRIFIED As 1 Million+ FLOODLondon to Mourn Charlie Kirk',
+        youtuber: 'StateOfDaniel',
+        stats: '1.2M views • 22 hours ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/E0mZ2R2GBl4',
+    channelPicture: 'Interface/channel-pictures/alpha.png',
+    videoInfo: {
+        title: 'From Game Addict to Successful',
+        youtuber: 'Low Budget Stories',
+        stats: '809K views • 6 months ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/nruN-sJXJxo',
+    channelPicture: 'Interface/channel-pictures/sigma.png',
+    videoInfo: {
+        title: 'College Majors be like...',
+        youtuber: 'The Crusader',
+        stats: '113K views • 12 days ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/uHt01D6rOLI',
+    channelPicture: 'Interface/channel-pictures/joma.png',
+    videoInfo: {
+        title: 'If American Psycho were about Programmers',
+        youtuber: 'Joma Tech',
+        stats: '5M views • 5 years ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/n0VG8lj6hd4',
+    channelPicture: 'Interface/channel-pictures/sarah-wu.png',
+    videoInfo: {
+        title: 'unemployed at 24 | life 2 years after MIT',
+        youtuber: 'Sarah Wu',
+        stats: '13K views • 3 weeks ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/gCpR4ODSE6s',
+    channelPicture: 'Interface/channel-pictures/christmas.png',
+    videoInfo: {
+        title: 'IanOnYouTube',
+        youtuber: 'Day in the life working a dead end retail job at 30',
+        stats: '481K views • 3 months ago'
+    }
+    }, {
+    url: 'https://youtube.com/embed/yFbrK_sRV9Q',
+    channelPicture: 'Interface/channel-pictures/x.png',
+    videoInfo: {
+        title: 'Elon\'s SpaceX Tour - Offices',
+        youtuber: 'SpaceX',
+        stats: '7.2M views • 14 years ago'
+    }
+    }, {
+        url: 'https://youtube.com/embed/Cr9B6yyLZSk',
+        channelPicture: 'Interface/channel-pictures/mrwhoistheboss.png',
+        videoInfo: {
+            title: 'iPhone 17/Air/Pro Hands on - Everything’s changed',
+            youtuber: 'Mrwhosetheboss',
+            stats: '8.1M views • 4 days ago'
+        }
+    }];
 
