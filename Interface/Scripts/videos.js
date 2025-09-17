e0c34ae4720d46c817e0c4df9df51689ea89be9f
Makky <teniolaa890@gmail.com>
2025-09-17 13:38:39 +0100

    update-1

---
+++ Interface/Scripts/videos.js
@@ -0,0 +1,10 @@
+export const videos = [{
+    url: 'https://youtube.com/embed/4vKy2RGd0e4',
+    channelPicture: 'Interface/channel-pictures/folder.png',
+    videoInfo: {
+        title: 'The Hacker Who Built a Ransomware Empire',
+        youtuber: 'Blackfiles',
+        stats: '63K views • 11 days ago'
+    }
+}];
+
